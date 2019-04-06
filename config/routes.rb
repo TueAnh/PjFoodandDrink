ff076b286a9cb273e8e80957ef4c18d2c9f6c013
TueAnh <taubeopro@gmail.com>
2019-04-06 10:26:44 +0700

    Add Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+	root "static_pages#home"
   devise_for :users
+   as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+    get "signup" => "devise/registrations#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
