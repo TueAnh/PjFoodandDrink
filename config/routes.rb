2b1648044482fbde293c0c724f6a47077d5b5919
TueAnh <taubeopro@gmail.com>
2019-04-06 23:39:01 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	root "static_pages#home"
+	
   devise_for :users
+  resources :users, :only => [:show]
    as :user do
     get "signin" => "devise/sessions#new"
     post "signin" => "devise/sessions#create"
