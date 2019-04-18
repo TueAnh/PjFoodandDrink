097dc6edf7bd62e1b3dd2ee3f192bc89f5b5e272
BuBuHuy <luuvanhuy2012@gmail.com>
2019-04-18 15:39:02 +0700

    PushChatt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'messages/index'
+
+  get 'conversations/index'
+
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+
+    resources :messages, only: [:create]
+  end
 	root "static_pages#home"
   devise_for :users
   resources :products, only: [:index, :show]
